317a7f52254d2b4fd430916250d1a27429364784
Jeferson <comerciall.jeferson@gmail.com>
2020-07-31 22:10:17 -0300

    crawler working fine with seach and limit params

--- middlewares/items.js
+++ middlewares/items.js
@@ -1,53 +1,60 @@
-var Crawler = require("crawler");
+var cheerio = require("cheerio");
+var axios = require("axios");
 
-function searchItems(req, response, next) {
+async function searchItems(req, response, next) {
+    var limit = req.body.limit
     
+    //url of website
     var URL = 'https://lista.mercadolivre.com.br/'
 
-    var c = new Crawler({
-        maxConnections : 10,
-        
-        callback : function (error, res, done) {
-            if(error){
-                console.log(error);
-            }else{
-                //cheerio is $ to emulate a jquery reference
-                var $ = res.$;
+    //create an empty array of items
+    var items = []
 
-                //create an empty array of items
-                var items = []
-                
-                $(".results-item", "#searchResults").each(function(i, elem) {
-                    //getting price
-                    var priceValue = $(this).find(".price__fraction").text()
-                    var priceDecimal = $(this).find(".price__decimals").text()
-                    
-                    //getting title
-                    var title = $(this).find(".main-title").text().trim()
-                    //getting link
-                    var link = $(this).find("a").attr('href')
-                    //parsing price as float
-                    var price = parseFloat(`${priceValue}.${priceDecimal}`)
+    //condition to stop scraping the website
+    while(items.length < limit){
+        //you can jump the number of items that you already seen with the string "_Desde_51" for example
+        var fullUrl = URL+req.body.search.split(" ").join("-")+`_Desde_${items.length}`
+        console.log(fullUrl)
 
-                    //adding items to array
-                    items.push({
-                        name: title,
-                        link: link,
-                        price: price,
-                        store: "",
-                        state: ""
-                    });
-                });
+        var result = await axios.get(fullUrl)
 
-                response.data = items
+        const $ = cheerio.load(result.data)
+
+        //get each element of the list of products
+        $(".results-item", "#searchResults").each(function(i, elem) {
+            //getting price
+            var priceValue = $(this).find(".price__fraction").text()
+            var priceDecimal = $(this).find(".price__decimals").text()
             
-                next()
-            }
-            done();
-        }
-    });
+            //getting title
+            var title = $(this).find(".main-title").text().trim()
+            //getting link
+            var link = $(this).find("a").attr('href')
+            //parsing price as float
+            var price = parseFloat(`${priceValue}.${priceDecimal}`)
 
-    c.queue(URL+req.body.search.split(" ").join("-"));
+            //getting the state
+            var state = $(this).find(".item__condition").text().trim()
+
+            //adding items to array
+            items.push({
+                name: title,
+                link: link,
+                price: price,
+                store: "",
+                state: state
+            });
+
+            //when the quantity of items reach the limit, stop adding in array
+            if(items.length == limit)
+                return false
+        });
+    }
+
+    //add items to the response
+    response.data = items
+
+    next()
 }
 
 module.exports = {
